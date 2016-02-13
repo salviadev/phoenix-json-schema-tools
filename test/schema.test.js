5633fa264b6fdd13c63bf96491136e3953a7a91b
misto <mihaistoie@gmail.com>
2016-02-13 12:53:15 +0100

    #

--- test/schema.test.js
+++ test/schema.test.js
@@ -1,41 +1,68 @@
 var assert = require("assert");
-var indexesOfSchema = require("../index").indexesOfSchema;
+var pschema = require("../index").schema;
 describe('Schema indexes', function () {
     describe('#indexes()', function () {
         it('Extract indexes', function () {
-        var schema = {
+            var schema = {
                 name: "users",
                 primaryKey: "id",
                 indexes: [
-                   {fields: "firstName,lastName", unique: true},
-                   {fields: "age"}
+                    { fields: "firstName,lastName", unique: true },
+                    { fields: "age" }
                 ],
                 properties: {
-                    id: {type: "string"},
-                    firstName : {type: "string"},
-                    lastName : {type: "string", capabilities: "searchable"},
-                    age : {type: "number"},
+                    id: { type: "string" },
+                    firstName: { type: "string" },
+                    lastName: { type: "string", capabilities: "searchable" },
+                    age: { type: "number" },
                     address: {
                         type: "object",
-                        indexes: [{fields: "street"}],
+                        indexes: [{ fields: "street" }],
                         properties: {
-                            street : {type: "string", capabilities: "searchable"}
-                            
+                            street: { type: "string", capabilities: "searchable" }
+
                         }
-                    } 
+                    }
                 }
-            };            
-            var indexes = indexesOfSchema(schema, true);
+            };
+            var indexes = pschema.indexesOfSchema(schema, true);
             assert.deepEqual(indexes, [
-                    {fields: "id", unique: true}, 
-                    {fields: "firstName,lastName", unique: true}, 
-                    {fields: "age", unique: false}, 
-                    {fields: "address.street", unique: false},
-                    {fields: "lastName,address.street", text: true}
-                    ]);
+                { fields: "id", unique: true },
+                { fields: "firstName,lastName", unique: true },
+                { fields: "age", unique: false },
+                { fields: "address.street", unique: false },
+                { fields: "lastName,address.street", text: true }
+            ]);
 
         });
+
+    });
+    describe('#types()', function () {
         
+        it('typeOf', function () {
+            var   schema2 = {
+            "name": "SPO_OPERATION",
+            "title": "Opérations",
+            "primaryKey": "idop",
+            "indexes": [{
+                "unique": false,
+                "fields": "commune"
+            }],
+            "properties": {
+                "idop": {
+                    "title": "Id Op",
+                    "type": "string"
+                },
+                "dtfinphase2": {
+                    "type": "date"
+                }
+            }
+        };
+            var tt = pschema.typeOfProperty('dtfinphase2', schema2);
+            assert.equal(tt, 'date');
+            
+        });
+
     });
 
 })
